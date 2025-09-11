bb094f0258535cdbee96d9189ac1c9a916434cc7
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-09-12 01:04:05 +0530

    implement the functions of food add page in the admin-panel

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,30 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Add = () => {
+
+  const url = "http://localhost:4000";
+  const [image,setImage] =useState(false);
+  const [data,setData] = useState({
+    name:"",
+    description:"",
+    category:"Salad",
+    price:"",
+  })
+
+  //create the data change function
+  const onChangeHandler = (event) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  //create the API call function
+
+  const onSubmitHandler = async (event)=>{
+    event.preventDefault(); // this is use for when we press the add button rellod the page the details are erase so use preventDefault for stop the reload
+
+    //then we update the form data
+    const formData = new FormData();
+    formData.append("name",data.name)
+    formData.append("description",data.description)
+    formData.append("price",Number(data.price)) // use Number for convert string to number
+    formData.append("category",data.category)
+    formData.append("image",image)
+    //api call using axios
+    const response = await axios.post(`${url}/api/food/add`,formData) // this is the url of pass the form data ${url}/api/food/add
+    if(response.data.success){
+      setData({
+        name:"",
+        description:"",
+        category:"Salad",
+        price:"",
+      })
+      setImage(false) //in this case if the data is successfully add an refresh the data
+
+      //tost notification in successful
+      toast.success(response.data.message)
+    }
+    else{
+      toast.error(response.data.message)
+    }
+  }
+
+  useEffect(()=>{
+    console.log(data);
+  },[data])
+
   return (
     <div className='add'>
-      <form className='flex-col'>
+      <form className='flex-col' onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image" className="">
-            <img src={assets.upload_area} alt="" />
+            <img src={image?URL.createObjectURL(image):assets.upload_area} alt="" />
           </label>
-          <input type="file" id='image' hidden required />
+          <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
-          <input type="text" name='name' placeholder='Type here' />
+          <input onChange={onChangeHandler} value={data.name} type="text" name='name' placeholder='Type here' />
         </div>
         <div className="add-product-description flex-col">
           <p>Product Description</p>
-          <textarea name="description" rows="6" placeholder='write content here'></textarea>
+          <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder='write content here'></textarea>
         </div>
         <div className="add-category-price">
           <div className="add-category flex-col">
             <p>Product Category</p>
-            <select name="category" id="">
+            <select onChange={onChangeHandler} value={data.category}name="category" id="">
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Deserts">Deserts</option>
@@ -37,7 +91,7 @@ const Add = () => {
           </div>
           <div className="add-price flex-col">
             <p>Product Price</p>
-            <input type="number" name='price' placeholder='$20'/>
+            <input onChange={onChangeHandler} value={data.price} type="number" name='price' placeholder='$20'/>
           </div>
         </div>
         <button type='submit' className='add-btn'>ADD</button>
